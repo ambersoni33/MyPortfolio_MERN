8d1e763238b587d3cf7ad08c4768e2b3fead7222
Amber Soni <66764811+ambersoni33@users.noreply.github.com>
2025-04-28 23:51:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require('path');
 
 //dotenv configuartion
 dotenv.config();
@@ -13,17 +12,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'client', 'build')))
-
+app.use(express.static(path.join(__dirname,'./client/build')));
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 
 app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
-
 //port
+
 const PORT = process.env.PORT || 8080;
 
 //listen
